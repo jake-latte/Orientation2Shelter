a040c9ebcbef33d40d47a9760e1e1fe3ccd20d70
jlah5331 <jlah5331@uni.sydney.edu.au>
2025-02-22 15:57:32 +0000

    plane

--- Tasks/PLANE.py
+++ Tasks/PLANE.py
@@ -9,8 +9,7 @@ from test_funcs import *
 default_params = {
     'min_xy': -1,
     'max_xy': 1,
-    'max_xy_per_trial': 20,
-    'pulse_length': 5
+    'init_duration': 10
 }
 
 input_map = {
@@ -24,44 +23,21 @@ target_map = {
 }
 
 def create_data(config, inputs, targets, mask):
+    batch_size, n_timesteps = inputs.shape[0], inputs.shape[1]
+    init_duration = config.init_duration
+
+    x_vals = (config.max_xy-config.min_xy)*torch.rand((batch_size,)) + config.min_xy
+    y_vals = (config.max_xy-config.min_xy)*torch.rand((batch_size,)) + config.min_xy
+
+    inputs[:,:init_duration,input_map['x']] = x_vals.reshape((batch_size,1)).repeat((1,init_duration))
+    inputs[:,:init_duration,input_map['y']] = y_vals.reshape((batch_size,1)).repeat((1,init_duration))
+
+    mask[:,:init_duration] = False
+
+    targets[:,:,target_map['x']] = x_vals.reshape((batch_size,1)).repeat((1,n_timesteps))
+    targets[:,:,target_map['y']] = y_vals.reshape((batch_size,1)).repeat((1,n_timesteps))
     
-    for trial in range(inputs.shape[0]):
-        n_x = torch.randint(0, config.max_xy_per_trial, (1,))
-        n_y = torch.randint(0, config.max_xy_per_trial, (1,))
-
-        x_times = torch.randint(0, config.n_timesteps, (n_x,)).sort()[0]
-        x_vals = (config.max_xy-config.min_xy)*torch.rand((n_x,)) + config.min_xy
-
-        y_times = torch.randint(0, config.n_timesteps, (n_y,)).sort()[0]
-        y_vals = (config.max_xy-config.min_xy)*torch.rand((n_y,)) + config.min_xy
-
-        x_out = torch.zeros((config.n_timesteps,))
-        y_out = torch.zeros((config.n_timesteps,))
-
-        for i in range(n_x):
-            this_time = x_times[i]
-            inputs[trial, this_time:this_time+config.pulse_length, input_map['x']] = x_vals[i]
-
-            if i < n_x-1:
-                next_time = x_times[i+1]
-                x_out[this_time+config.pulse_length:next_time] = x_vals[i]
-            else:
-                x_out[this_time+config.pulse_length:] = x_vals[i]
-
-        for i in range(n_y):
-            this_time = y_times[i]
-            inputs[trial, this_time:this_time+config.pulse_length, input_map['y']] = y_vals[i]
-
-            if i < n_y-1:
-                next_time = y_times[i+1]
-                y_out[this_time+config.pulse_length:next_time] = y_vals[i]
-            else:
-                y_out[this_time+config.pulse_length:] = y_vals[i]
-
-            targets[trial, :, target_map['x']] = x_out
-            targets[trial, :, target_map['y']] = y_out
-
-    vars = {}
+    vars = {'x': x_vals, 'y': y_vals}
 
 
     return inputs, targets, vars, mask
